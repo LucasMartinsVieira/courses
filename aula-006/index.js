6d94df84fb9d8e57909704251be276434c4dbd76
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-10 20:07:27 -0300

    Aula 006

---
+++ aula-006/index.js
@@ -0,0 +1,17 @@
+let nome = "João";
+
+console.log(nome, 'Nasceu em 2003');
+console.log('Em 2013', nome, 'conheceu Maria');
+console.log('Em 2023', nome, 'foi jogar no vascão');
+
+nome = "Lucas";
+
+console.log(nome, 'Nasceu em 2003');
+console.log('Em 2013', nome, 'conheceu Maria');
+console.log('Em 2023', nome, 'foi jogar no vascão');
+
+nome = 10;
+
+console.log(nome, 'Nasceu em 2003');
+console.log('Em 2013', nome, 'conheceu Maria');
+console.log('Em 2023', nome, 'foi jogar no vascão');
