540c6a3f738c8b7d2ba6e1709d19ebb347d91a4f
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-10 20:14:23 -0300

    Aula 007

---
+++ aula-007/index.js
@@ -0,0 +1,14 @@
+const nome = "Lucas";
+
+console.log(nome, 'Nasceu em 2003');
+console.log('Em 2013', nome, 'conheceu Maria');
+console.log('Em 2023', nome, 'foi jogar no vascão');
+
+const primeiroNumero = 5; 
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoTriplicado = resultado * 3
+
+console.log(resultado);
+console.log(resultadoTriplicado);
+console.log(typeof(primeiroNumero));
