b0885ee8a54099f30eade8c128b91dafb93ba28a
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-10 19:22:06 -0300

    Aula 003

---
+++ aula-003/ex/index.js
@@ -0,0 +1,5 @@
+// const nome = "Lucas";
+// const horario = 19;
+
+// console.log("Meu nome é. Estou aprendendo JavaScript às ")
+console.log('Meu nome é "Lucas". Estou apreendendo JavaScript às', 19, 'da noite');
