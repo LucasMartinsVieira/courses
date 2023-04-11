d278445535c95dccc1e0ee6135bac4f04b1a2034
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-11 19:42:44 -0300

    Aula 010 + Modelo

---
+++ aula-010/assets/js/main.js
@@ -0,0 +1,31 @@
+/**
+* Aritiméticos
+* + Adição / Concatenação
+* ** Potenciação
+* % Resto da divisão
+* () ** / * % + - 
+* NaN not a number
+*/
+
+let num1 = 10;
+let num2 = 3;
+
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+// Incremento / Decremento
+// num2--;
+++num2;
+console.log(num2);
+
+num1 = num1 + num2;
+// num1 += num2
+num1 **= num2
+console.log(num1);
+
+const num3 = 10;
+// const num4 =  parseInt('3');
+// const num4 =  parseFloat('3.14');
+const num4 =  Number('3.14');
+
+console.log(num3 + num4);
