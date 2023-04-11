4e826197c652a009e53b8a2a14996831c2b3ce69
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-11 19:14:28 -0300

    Aula 009

---
+++ aula-009/index.js
@@ -0,0 +1,30 @@
+// Yep, but var sucks
+// var nome = "lucas";
+// var nome = "dinheiro";
+
+// Nop
+// let nome = "lucas"
+// let nome = "dinheiro"
+
+// console.log(nome);
+
+// Primitive Types
+
+const nome = "Lucas"; //String
+const nome1 = "Lucas";
+const nome2 = "Lucas";
+
+const num1 = 10; // Number
+const num2 = 10.28;
+
+let qualquer; // Undefined = não aponta pra nenhum na memória
+let qualquer2 = null; // Nulo -> não aponta pra nenhum na memória. Não igual ao undefined
+
+const bool = true; // Boolean: True or False
+
+console.log(typeof(nome));
+console.log(typeof(num1));
+console.log(typeof(qualquer));
+console.log(typeof(qualquer2));
+console.log(typeof(bool));
+
