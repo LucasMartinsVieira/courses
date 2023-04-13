190c0ffc9c064c28826b834a2265d5b4c577d8ee
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-13 19:04:30 -0300

    Aula 014

---
+++ aula-014/assets/js/main.js
@@ -0,0 +1,46 @@
+// IEEE 754-2008
+
+let num1 = 1500;
+let num2 = 25.73829173912793;
+
+let num3 = 0.7;
+let num4 = 0.1;
+
+num3 = ((num3 * 100) + (num4 * 100)) / 100; // 0.8
+// num3 = ((num3 * 100) + (num4 * 100)) / 100; // 0.9
+// num3 = ((num3 * 100) + (num4 * 100)) / 100; // 1.0
+
+console.log(num3);
+
+// console.log(num3 + num4);
+
+// num3 += num4; // 0.8
+// num3 += num4; // 0.9
+// num3 += num4; // 1.0
+// num3 += num4; // 1.1
+// num3 += num4; // 1.2
+// num3 += num4; // 1.3
+// num3 += num4; // 1.4
+// num3 += num4; // 1.5
+// num3 += num4; // 1.6
+// num3 += num4; // 1.7
+// num3 += num4; // 1.8
+// num3 += num4; // 1.9
+// num3 += num4; // 2.0
+
+// num3 = num3.toFixed(2);
+// num3 = parseFloat(num3.toFixed(2));
+//
+// console.log(num3);
+// console.log(Number.isInteger(num3));
+
+// num1 = num1.toString;
+
+// console.log(num1.toString() + num2);
+// console.log(num1.toString(2));
+// console.log(num2.toFixed(2));
+// console.log(Number.isInteger(num1));
+
+// let temp = num1 * 'Ola';
+// let temp = num1 * '5';
+// console.log(Number.isNaN(temp));
