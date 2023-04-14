9078ad0dc32ec2ec47508a1120f93545aa978b90
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-14 19:39:13 -0300

    Aula 016

---
+++ aula-016/assets/js/main.js
@@ -0,0 +1,28 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+// const alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array)
+
+// alunos.push('Luíza');
+// alunos.push('Dudu');
+// console.log(alunos.slice(0, -2));
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// const removido = alunos.pop(); // Remove elemento do final
+// console.log(removido);
+
+// alunos.unshift('Luíza'); // Adiciona no inicio do array.
+// alunos.unshift('Johnatan'); // Adiciona no inicio do array.
+
+// alunos.push('Luíza'); // Adiciona no fim do array.
+// alunos.push('Luana');
+
+// alunos[alunos.length] = 'Luíza';
+// alunos[alunos.length] = 'Luana';
+
+// console.log(alunos.length);
+// alunos[0] = "Lucas"; // Alteara
+// console.log(alunos[0]);
