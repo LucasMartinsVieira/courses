12e977ada695bf0d82c796d8cea53fd0a5b8aa07
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-15 19:15:37 -0300

    Aula 022

---
+++ aula-022/assets/js/main.js
@@ -0,0 +1,18 @@
+/*
+ * Operadores Lógicos
+ * && -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+ * || -> OR -> OU ->
+ * ! -> NOT -> NÃO ->
+ */
+
+// const expressaoAnd = true && true && true;
+// const expressaoOr = true || false;
+// const usuario = 'Luiz'
+// const senha = '123456'
+//
+// const vaiLogar = usuario === 'Luiz' && senha === '12345'
+// console.log(vaiLogar)
+
+// console.log(expressaoOr)
+// console.log(!true);
+// console.log(!!true);
