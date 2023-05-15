4825e08bf5ca0638b4a6991459dddbb28d3f2aac
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-15 19:04:06 -0300

    Aula 021

---
+++ aula-021/assets/js/main.js
@@ -0,0 +1,15 @@
+/* Operadores de comparação
+> Maior que
+>=Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== igualdade (valor) **************
+=== igualdade estrita (valor e tipo)
+!= diferent (valor) ****************
+!== diferent estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = '10';
+
+const comp = num1 == num2;
+csole.log(comp);
