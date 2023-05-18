a761914056569646f9d967ad50a8cf5b1f208023
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-18 19:07:36 -0300

    Aula 025

---
+++ aula-025/assets/js/main.js
@@ -0,0 +1,19 @@
+// const nome = "Lucas";
+//
+// if (nome == "Lucas") {
+//   console.log(`Seu nome é ${nome}`);
+// } else {
+//   console.log(`Coe josé`);
+// }
+
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+  console.log(`O número: ${numero} está entre 0 e 5`)
+} else if (numero >= 6 && numero <= 8) {
+  console.log(`O número: ${numero} está entre 6 e 8`)
+} else if (numero >= 9 && numero <= 11) {
+  console.log(`O número: ${numero} está entre 9 e 11`)
+} else {
+  console.log(`O número: ${numero} NÃO está entre 0 e 11`)
+}
