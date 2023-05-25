9e17b3072c5026f907dda5e6e6434b1932941287
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-25 08:52:18 -0300

    Aula 028

---
+++ aula-028/assets/js/main.js
@@ -0,0 +1,27 @@
+// const tresHoras = 60 * 60 * 3 * 1000
+// const umDia = 60 * 60 * 24 * 1000
+// const data = new Date(0 + tresHoras + umDia);
+
+// const data = new Date(2019, 0); // Ano, Mês, Dia, Hora, Minuto, Segundo, Milisegundos
+// const data = new Date('2003-11-29 17:21:59.100');
+
+// console.log(Date.now());
+
+function zeroAEsquerda(num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());               
+  const min = zeroAEsquerda(data.getMinutes());
+  const seg = zeroAEsquerda(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
