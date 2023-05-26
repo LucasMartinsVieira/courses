c77e4f430195d6acb76c4928471b128dea2b2482
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-26 09:41:12 -0300

    Aula 034

---
+++ aula-034/assets/js/main.js
@@ -0,0 +1,21 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// i - index
+
+const frutas = ['Maça', 'Pêra', 'Uva', 'Morango', 'Manga', 'Chia', 'Melancia']
+
+// console.log(frutas.length);
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Índice ${i}`,frutas[i]);
+}
+
+// for (let i = 0; i <= 10; i++) {
+//   const par = i % 2 === 0 ? 'par' : 'impar'
+//   console.log(i, par)
+// }
