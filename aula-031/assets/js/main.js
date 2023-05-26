1a63aefe83cf97ad804f9c8a3d4a190947f65aab
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-26 08:31:20 -0300

    Aula 031

---
+++ aula-031/assets/js/main.js
@@ -0,0 +1,41 @@
+const vedadeira = true
+
+// let tem escopo de bloco { ... }
+// var só tem escopo de funçãa
+
+// let nome = 'Lucas';
+// var nome2 = 'Lucas';
+//
+// if (vedadeira) {
+//   let nome = 'Martins'
+//   console.log(nome, nome2);
+//
+//   if (vedadeira) {
+//     let nome = 'Something else';
+//     console.log(nome, nome2);
+//   }
+// }
+
+// function falaOi() {
+//   if (vedadeira) {
+//     let nome = 'Lucas'
+//     var sobrenome = 'Martins'
+//     console.log(nome)
+//   }
+//   console.log(sobrenome)
+// }
+//
+// // console.log(nome);
+// falaOi()
+
+// const funcaoAnonima = () => {
+//   console.log('lucas')
+// }
+//
+// const somar = (a, b) => a + b;
+// console.log(somar(5, 6))
+
+// funcaoAnonima();
+
+let sobrenome = 'Martins';
+console.log(sobrenome);
