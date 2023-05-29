ff53afcfb48b7dbfeb5f7471ab6f71a72f50ac08
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-29 17:24:59 -0300

    Aula 041

---
+++ aula-041/assets/js/main.js
@@ -0,0 +1,12 @@
+const maiorNumero = (x, y) => {
+  return x > y ? x : y;
+}
+
+const maiorNumero2 = (x, y) => {
+  return x > y ? x : y;
+}
+
+const maiorNumero3 = (x, y) => x > y ? x : y;
+
+
+console.log(maiorNumero3(100, 20));
