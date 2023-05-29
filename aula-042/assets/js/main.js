9a01aa30b483c1c3f978c6e06dcfb8e05255534b
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-29 17:25:11 -0300

    Aula 042

---
+++ aula-042/assets/js/main.js
@@ -0,0 +1,8 @@
+// const ePaisagem = (largura, altura) => {
+//   return largura > altura ? true : false
+// }
+
+const ePaisagem = (largura, altura) => largura > altura
+
+// console.log(ePaisagem(1920, 1080));
+console.log(ePaisagem(1080, 1920));
