46454fcdc26b4381da62745b57b5d6f9e99462a1
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-05-30 10:14:01 -0300

    Aula 046

---
+++ aula-046/assets/js/main.js
@@ -0,0 +1,23 @@
+const mostraHora = () => {
+  let data = new Date();
+
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+  });
+}
+
+const funcaoDoInterval = () => {
+  console.log(mostraHora());
+}
+
+const timer = setInterval(() => {
+  console.log(mostraHora())
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(timer);
+}, 5000);
+
+setTimeout(() => {
+  console.log('Olá, Mundo!')
+}, 5000);
