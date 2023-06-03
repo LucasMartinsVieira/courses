b626884aeca43575dc9ebb63606ee93989a64a23
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-03 09:14:16 -0300

    Aula 062

---
+++ aula-062/assets/js/main.js
@@ -0,0 +1,38 @@
+// Valor por referêrencia
+// const nomes = new Array('Lucas', 'Maria', 'João');
+// const nomes = [ 'Lucas', 'Maria', 'João', 'Wallace', 'Rosana' ];
+// const novo = nomes;
+// const novo = [...nomes];
+
+// nomes[2] = 'Joana'
+// delete nomes[2];
+// novo.pop();
+// console.log(nomes);
+// console.log(novo);
+
+// console.log(nomes.length);
+// const removido = nomes.pop();
+// const removido = nomes.shift();
+// console.log(removido);
+
+// nomes.push('Joana');
+// nomes.push('Wallace');
+
+// nomes.unshift('Joana');
+// nomes.unshift('Wallace');
+
+// const novo = nomes.slice(0, -1);
+//
+// console.log(novo)
+// console.log(nomes);
+
+// const nome = 'Lucas Martins Vieira';
+//
+// const nomes = nome.split(' ');
+
+const nomes = [ 'Lucas', 'Martins', 'Vieira' ];
+const nome = nomes.join(' ');
+
+console.log(nome);
+// console.log(nomes);
+
