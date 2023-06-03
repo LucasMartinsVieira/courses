84a12744b49c8035b043912165ee033f70df0da2
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-03 09:38:58 -0300

    Aula 063

---
+++ aula-063/assets/js/main.js
@@ -0,0 +1,28 @@
+//               -5        -4      -3         -2        -1
+//                0        1        2          3         4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
+
+// nomes.splice(indice, delete, elemento1, elemento2, elemento3);
+
+// pop
+// const removidos = nomes.splice(-1, 1);
+
+// shift
+// const removidos = nomes.splice(0, 1);
+
+// push
+// nomes.splice(nomes.length, 0, 'Lucas', 'Martins', 'Vieira');
+
+// unshift
+// nomes.splice(0, 0, 'Lucas');
+
+// const removidos = nomes.splice(3, 2);
+// const removidos = nomes.splice(-2, 0);
+// const removidos = nomes.splice(-2, Number.MAX_VALUE)
+// const removidos = nomes.splice(3, 0, 'Lucas');
+// const removidos = nomes.splice(3, 2, 'Lucas', 'Martins');
+console.log(nomes)
+console.log(removidos)
+
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
