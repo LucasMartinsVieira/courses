cd5a182c2a53107f5169ab1ecf9222d8893037d9
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-09 19:53:21 -0300

    Aula 096 - Geradora de Senhas

---
+++ aula-096/src/modules/formGeraSenha.js
@@ -0,0 +1,29 @@
+import geraSenha from "./geradores"
+
+const senhaGerada = document.querySelector('.senha-gerada')
+const qtdCaracteres = document.querySelector('.qtd-caracteres')
+const chkMaisculas = document.querySelector('.chk-maiusculas')
+const chkMinusculas = document.querySelector('.chk-minusculas')
+const chkNumeros = document.querySelector('.chk-numeros')
+const chkSimbolos = document.querySelector('.chk-simbolos')
+const gerarSenha = document.querySelector('.gerar-senha')
+
+
+export default () => {
+  gerarSenha.addEventListener('click', () => {
+    senhaGerada.innerHTML = gera()
+  })
+  console.log('Ola mundão')
+}
+
+function gera() {
+  const senha = geraSenha(
+    qtdCaracteres.value,
+    chkMaisculas.checked,
+    chkMinusculas.checked,
+    chkNumeros.checked,
+    chkSimbolos.checked
+  )
+
+  return senha || 'Nada Selecionado'
+}
