8603d020941a849bb3b0d27a0a34bb0edbe226fa
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-10 20:46:34 -0300

    Node: Aula 005 - Escrevendo e Lendo arquivos com fs

---
+++ node/aula-005/app.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const caminhoArquivo = path.resolve(__dirname, 'teste.json')
+const escreve = require('./modules/escrever')
+const ler = require('./modules/ler')
+
+// const pessoas = [
+//   { nome: 'João' },
+//   { nome: 'Maria' },
+//   { nome: 'Eduardo' },
+//   { nome: 'Luiza' },
+// ]
+//
+// const json = JSON.stringify(pessoas, '', 2)
+// escreve(caminhoArquivo, json)
+
+async function leArquivo(caminho) {
+  const dados = await ler(caminho)
+  renderizaDados(dados)
+  // return dados
+}
+
+function renderizaDados(dados) {
+  dados = JSON.parse(dados)
+
+  dados.forEach(val => console.log(val.nome));
+}
+
+leArquivo(caminhoArquivo)
+
+// const dadosArquivo = leArquivo(caminhoArquivo)
+//   .then((dados) => console.log(dados))
+//   .catch((e) => console.log(e))
