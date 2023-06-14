355ebeba4e5ba070253bd22be362ea1f66410c55
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-14 18:59:30 -0300

    PA: Signing up users

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const route = express.Router()
-const homeController = require('./src/controllers/homeController')
-const loginController = require('./src/controllers/loginController')
+const express = require('express');
+const route = express.Router();
 
-// Rotas da Home
-route.get('/', homeController.index)
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+// Rotas da home
+route.get('/', homeController.index);
 
 // Rotas de login
-route.get('/login/index', loginController.index)
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
-module.exports = route
+module.exports = route;
