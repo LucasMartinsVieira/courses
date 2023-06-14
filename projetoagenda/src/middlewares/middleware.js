03b30869121338164c50e3654d8d05a0a7ef8114
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-14 17:24:37 -0300

    PA: Creating login page

--- projetoagenda/src/middlewares/middleware.js
+++ projetoagenda/src/middlewares/middleware.js
@@ -9,10 +9,11 @@ exports.outroMiddleware = (req, res, next) => {
 }
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if (err && err.code == 'EBADCSRFTOKEN') {
-    // return res.send('BAD CSRF.')
+  if (err) {
     return res.render('404')
   }
+
+  next()
 }
 
 exports.csrfMiddleware = (req, res, next) => {
