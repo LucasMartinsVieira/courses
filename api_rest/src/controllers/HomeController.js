5d464d1f4e86b033f4441e9d3995c28facee286d
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-19 19:15:30 -0300

    API_REST: Alunos routes

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Lucas',
-      sobrenome: 'Martins',
-      email: 'lucas@gmail.com',
-      idade: 19,
-      peso: 75,
-      altura: 1.7,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
