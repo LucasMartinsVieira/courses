fce1d55695677768e1915fef8873987800b973e4
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-22 09:34:41 -0300

    API_REST: Adding CORS

--- api_rest/src/app.js
+++ api_rest/src/app.js
@@ -6,11 +6,26 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunosRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whitelist = ['https://localhost:3000'];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,9 +35,14 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
+    this.app.use(
+      '/images/',
+      express.static(resolve(__dirname, '..', 'uploads', 'images')),
+    );
   }
 
   routes() {
