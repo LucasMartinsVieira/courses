106d20115a6dd48deedb9974322d3d9359276f5c
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-06-27 08:46:56 -0300

    React: Routing

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
-import Login from './pages/login';
 import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </Router>
   );
 }
 
