81f634465de95926a95cadaca3d29069b620559e
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-07-06 17:13:07 -0300

    React: React-router-dom-v6

---
+++ react/react-router-dom-v6/src/components/Post/index.tsx
@@ -0,0 +1,8 @@
+import './style.css'
+
+export const Post = () => {
+    return <div>
+        <h1>Post</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi autem optio vel aliquid sequi, hic ratione provident similique pariatur rerum iusto dicta itaque deserunt vitae cum eius. Quis, quidem mollitia.</p>
+    </div>
+}
