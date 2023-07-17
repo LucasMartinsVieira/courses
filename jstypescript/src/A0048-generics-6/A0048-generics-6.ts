74071f742b211def25e441f73ada685dd7d6ad82
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-07-17 18:10:09 -0300

    TS: A048 - Generics 6

---
+++ jstypescript/src/A0048-generics-6/A0048-generics-6.ts
@@ -0,0 +1,10 @@
+export function joinObjects<T, U>(obj1: T, obj2: U): T & U {
+  // return { ...obj1, ...obj2 }
+  return Object.assign({}, obj1, obj2)
+}
+
+const obj1 = { key1: 'value 1' }
+const obj2 = { key2: 'value 2' }
+
+const union = joinObjects(obj1, obj2)
+console.log(union)
