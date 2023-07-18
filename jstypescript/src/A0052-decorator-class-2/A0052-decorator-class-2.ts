e071a48dd24896845570ffb067d456987c1bae98
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-07-18 10:04:31 -0300

    TS: A052 - Decorator class 2

---
+++ jstypescript/src/A0052-decorator-class-2/A0052-decorator-class-2.ts
@@ -0,0 +1,27 @@
+function reverseNameAndColor<T extends new (...args: any[]) => any>(target: T) {
+  console.log('I am the decorator e i received', target)
+  return class extends target {
+    name: string
+    color: string
+
+    constructor(...args: any[]) {
+      super(...args)
+      this.name = this.reverse(args[0])
+      this.color = this.reverse(args[1])
+    }
+
+    reverse(value: string): string {
+      return value.split('').reverse().join('')
+    }
+  }
+}
+
+@reverseNameAndColor
+export class Animal {
+  constructor(public name: string,public color: string) {
+    console.log('I am the Class')
+  }
+}
+
+const animal = new Animal('capybara','purple')
+console.log(animal)
