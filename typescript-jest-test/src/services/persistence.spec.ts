d48d5fe5805fe936d4c0009b98f2e9f6fee62fa8
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-07-19 19:54:02 -0300

    JEST: Initial setup

---
+++ typescript-jest-test/src/services/persistence.spec.ts
@@ -0,0 +1,13 @@
+describe('Testing something', () => {
+  it('it should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('Testing another thing', () => {
+  test('it should return Lucas', () => {
+    const name = 'Lucas';
+    expect(name).toBe('Lucas');
+  });
+});
