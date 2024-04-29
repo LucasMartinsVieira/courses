fefd4f73b808e852dac1f9cb072d66cb81d2cdfc
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-04-29 09:14:07 -0300

    java introduction video rocketseat

---
+++ Java/java-introduction/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public int salary;
+    protected String name;
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Java/java-introduction/src/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    void createMain() {
+        Main meuMain = new Main();
+        System.out.println(meuMain.name);
+        System.out.println(meuMain.salary);
+    }
+
