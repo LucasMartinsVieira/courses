7ff5b035eb29963d29425dc7c36380d0714b7e5d
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2024-07-05 20:49:28 -0300

    nestjs course: tenth section (typeorm)

---
+++ JavaScript/NestJS Fundamentos/api-typeorm/test/app.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { authRegisterDTO } from '../src/testing/auth-register-dto.mock';
+import { Role } from '../src/enums/role.enum';
+import dataSource from '../typeorm/data-source';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let accessToken: string;
+  let userId: number;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(() => {
+    app.close();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
